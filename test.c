b9990c6a4aa740798a83e42b05d8881ca9eef891
user-z2025 <3923649781@qq.com>
2025-12-29 16:12:46 +0800

    指针和数组

--- test.c
+++ test.c
@@ -1,7 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//
+//	printf("hello world!\n");
+//	return 0;
+//}
 #include <stdio.h>
 int main()
 {
-	printf("hello world!\n");
+	int arr1[4] = { 1,2,3,4 };
+	int arr2[4] = { 2,3,4,5 };
+	int arr3[4] = { 3,4,5,6 };
+	int* parr[3] = { arr1,arr2,arr3 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", parr[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
